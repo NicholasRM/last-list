041904d039e362d2e4f93f2c5634834086abb86d
= <=>
2024-04-17 11:46:38 -0400

    Added html to views.py

--- lastlist/lastlistweb/views.py
+++ lastlist/lastlistweb/views.py
@@ -1,24 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def login(request):
-    return HttpResponse("Login")
+    html = "<html><body>Login</body></html>"
+    return HttpResponse(html)
+
 
 def list_view(request):
-    return HttpResponse("list view")
+    html = "<html><body>List View</body></html>"
+    return HttpResponse(html)
+
 
 def inspect_list(request, list_id):
-    return HttpResponse("inspecting list...")
+    html = "<html><body>Inspecting List...</body></html>"
+    return HttpResponse(html)
+
 
 def item_search(request):
-    return HttpResponse("looking for items...")
+    html = "<html><body>Item Search...</body></html>"
+    return HttpResponse(html)
+
 
 def item_view(request, item_id):
-    return HttpResponse("Here's an item")
+    html = "<html><body>Item View</body></html>"
+    return HttpResponse(html)
+
 
 def vendor_search(request):
-    return HttpResponse("here's some vendors")
+    html = "<html><body>Vendor Search...</body></html>"
+    return HttpResponse(html)
+
 
 def vendor_view(request, vendor_id):
-    return HttpResponse("Here's a vendor with some items")
+    html = "<html><body>Vendor View</body></html>"
+    return HttpResponse(html)

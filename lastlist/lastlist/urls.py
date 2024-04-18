7fa9c5775f5fa7f716f878b1493be4a69125511e
Nicholas Mendes <nicholas_mendes@uri.edu>
2024-04-18 08:25:04 -0400

    Removed unnecessary path distinction for app

--- lastlist/lastlist/urls.py
+++ lastlist/lastlist/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("ll/", include("lastlistweb.urls")),
+    path("", include("lastlistweb.urls")),
     path('admin/', admin.site.urls),
 ]

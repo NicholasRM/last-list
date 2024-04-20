732b5047fa5f7d4aaa5d1594a8c4e3e8187573d2
= <=>
2024-04-20 09:49:00 -0400

    Created initial templates for application

--- lastlist/lastlistweb/views.py
+++ lastlist/lastlistweb/views.py
@@ -4,35 +4,28 @@ from django.http import HttpResponse
 
 # Create your views here.
 def login(request):
-    html = "<html><body>Login</body></html>"
-    return HttpResponse(html)
+    return render(request, "lastlistweb/login/index.html")
 
 
 def list_view(request):
-    html = "<html><body>List View</body></html>"
-    return HttpResponse(html)
+    return render(request, "lastlistweb/list-view/index.html")
 
 
 def inspect_list(request, list_id):
-    html = "<html><body>Inspecting List...</body></html>"
-    return HttpResponse(html)
+    return render(request, "lastlistweb/inspect-list/index.html")
 
 
 def item_search(request):
-    html = "<html><body>Item Search...</body></html>"
-    return HttpResponse(html)
+    return render(request, "lastlistweb/item-search/index.html")
 
 
 def item_view(request, item_id):
-    html = "<html><body>Item View</body></html>"
-    return HttpResponse(html)
+    return render(request, "lastlistweb/item-view/index.html")
 
 
 def vendor_search(request):
-    html = "<html><body>Vendor Search...</body></html>"
-    return HttpResponse(html)
+    return render(request, "lastlistweb/vendor-search/index.html")
 
 
 def vendor_view(request, vendor_id):
-    html = "<html><body>Vendor View</body></html>"
-    return HttpResponse(html)
+    return render(request, "lastlistweb/vendor-view/index.html")

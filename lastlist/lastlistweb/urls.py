5ee20ba9fd2af4472f92a873b884088cee15ab30
Nicholas Mendes <nicholas_mendes@uri.edu>
2024-04-21 19:58:24 -0400

    Start connecting pages to views

--- lastlist/lastlistweb/urls.py
+++ lastlist/lastlistweb/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from . import views
 
+app_name="lastlistweb"
+
 urlpatterns = [
-    path("", views.login, name="Sign In"),
-    path("signup/", views.signup, name="Sign Up"),
-    path("lists/", views.list_view, name="My Lists"),
-    path("lists/<int:list_id>/", views.inspect_list, name="A List"),
-    path("items/", views.item_search, name="Find Products"),
-    path("items/<int:item_id>/", views.item_view, name="Some Product"),
-    path("vendors/", views.vendor_search, name="Find Stores"),
-    path("vendors/<int:vendor_id>", views.vendor_view, name="Here's a vendor"),
+    path("", views.signin, name="signin"),
+    path("signout/", views.signout, name="signout"),
+    #path("signup/", views.signup, name="Sign Up"),
+    path("lists/", views.list_view, name="lists"),
+    path("lists/<int:list_id>/", views.inspect_list, name="listid"),
+    path("items/", views.item_search, name="items"),
+    path("items/<int:item_id>/", views.item_view, name="itemid"),
+    path("vendors/", views.vendor_search, name="vendors"),
+    path("vendors/<int:vendor_id>", views.vendor_view, name="vendorid"),
 ]
--- lastlist/lastlistweb/views.py
+++ lastlist/lastlistweb/views.py
@@ -1,34 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import List, Contains, Item, Vendor, Stock
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
-def login(request):
-    return render(request, "lastlistweb/login/index.html")
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+        return redirect('lastlistweb:signin')
+    else:
+        return render(request, "lastlistweb/login/index.html")
 
 
 def list_view(request):
-    return render(request, "lastlistweb/list-view/index.html")
+    lists = List.objects.filter(user_id=request.user.id)
+    return render(request, "lastlistweb/list-view/index.html", {'lists':lists})
 
 
 def inspect_list(request, list_id):
-    return render(request, "lastlistweb/inspect-list/index.html")
-
+    pass
+    lists = List.objects.get(user=request.user.id, pk=list_id)
+    if not lists:
+        redirect('list_view')
+    items = Contains.objects.get(list_id=list_id)
+    return render(request, "lastlistweb/inspect-list/index.html", {'items':items})
 
 def item_search(request):
+    pass
+    if request.method == 'POST':
+        prod_name = request.POST['prod_name']
+        items = Item.objects.filter(product__name__icontains=prod_name)
+        return render(request, "lastlistweb/item-search/index.html", {"items":items})
+        
     return render(request, "lastlistweb/item-search/index.html")
 
-
 def item_view(request, item_id):
-    return render(request, "lastlistweb/item-view/index.html")
+    pass
+    item = Item.objects.get(pk=item_id)
+    return render(request, "lastlistweb/item-view/index.html", {'item':item})
 
 
 def vendor_search(request):
+    pass
+    if request.method == 'POST':
+        vend_name = request.POST['vend_name']
+        vendors = Vendor.objects.filter(name__icontains=vend_name)
+        return render(request, "lastlistweb/vendor-search/index.html", {"vendors":vendors})
     return render(request, "lastlistweb/vendor-search/index.html")
 
 
 def vendor_view(request, vendor_id):
-    return render(request, "lastlistweb/vendor-view/index.html")
+    pass
+    vendor = Vendor.objects.get(pk=vendor_id)
+    inventory = Stock.objects.filter(vendor=vendor_id)
+    return render(request, "lastlistweb/vendor-view/index.html", {'vendor': vendor, 'inventory': inventory})
 
-def signup(request):
-    return HttpResponse("<html><body><h1>Change me</h1></body></html>")
+def signout(request):
+    logout(request)
+    return redirect("lastlistweb:signin")

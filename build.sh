af2f75fc9512276dabfe7a892cb2a652041c08b0
Nicholas Mendes <nicholas_mendes@uri.edu>
2024-04-29 18:28:08 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -o errexit
 pip install -r requirements.txt
-python ./lastlist/manage.py collectstatic
+python ./lastlist/manage.py collectstatic --no-input
 python ./lastlist/manage.py migrate

d255f357af12133d7bbab6696b535dea30e74d10
Nicholas Mendes <nicholas_mendes@uri.edu>
2024-04-29 18:38:56 -0400

    Add host

--- lastlist/lastlist/settings.py
+++ lastlist/lastlist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-#0m91k(18f^=xopuoipxd43#pa$t*(q$ry7g2s1xciuvt#bcg&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['last-list.onrender.com']
 
 
 # Application definition
